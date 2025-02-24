9de739dc0edeec8d6fd509482468b02322a37919
Mohab Khaled <mohabk044@gmail.com>
2025-02-24 18:31:15 +0200

    Part 06 ActionResult & Intro To Model Binding

--- Project_Structure/Controllers/MoviesController.cs
+++ Project_Structure/Controllers/MoviesController.cs
@@ -1,18 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_Structure.Models;
 
 namespace Project_Structure.Controllers
 {
     public class MoviesController : Controller
     {
-     
-        public void GetMovie(int id)
-        {
 
-        }  
-        
-        public string Index()
+
+        public IActionResult GetMovie(int id)
         {
-            return "Hello from Index";
+            if (id < 10)
+            {
+                return BadRequest();
+            }
+
+            if (id == 100) {
+                return NotFound();
+            }
+            //ContentResult result = new ContentResult();
+            //result.Content = $"Movie with id = {id}";
+            return Content($"Movie with id = {id}","text/html");
+
+            //BadRequestResult result02 = new BadRequestResult();
+            //return result02;
+            //UnauthorizedResult result03 = new UnauthorizedResult();
+            //return result03;
+            //ContentResult result = new ContentResult();
+            //result.Content = $"Movie with id = {id}";
+            //result.ContentType = "text/html";
+            //result.ContentType = "object/pdf";
+            //return result;
+            //return $"Movie with id = {id}";
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "Create"; 
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+          
         }
     }
 }
