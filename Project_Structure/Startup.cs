afb42e1f354f2a9469acbb7d0c6692d1be94b8a4
Mohab Khaled <mohabk044@gmail.com>
2025-02-24 04:16:57 +0200

    Part 04 Upgrade to .NET 8.0.mp4‏

--- Project_Structure/Startup.cs
+++ Project_Structure/Startup.cs
@@ -13,58 +13,7 @@ namespace Project_Structure
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();       //MVC
-            //services.AddRazorPages();
-            //services.AddControllers();   //API or MVC
-            //services.AddMvc();         //MIX
-        }  
+     
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-                //Static
-                endpoints.MapGet("/index", async context =>
-                {
-                    await context.Response.WriteAsync("Hello Route!");
-                });
-
-                ////Dynamic / Variable  
-                //endpoints.MapGet("/{id:int}", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello Route!");
-                //});
-                ////Mixed
-                //endpoints.MapGet("/Hello{id:alpha}", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello Route!");
-                //});
-
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Movies}/{action=Index}/{id:int?}"
-                    //defaults: new { Action = "Index" },
-                    //constraints: new { id = new IntRouteConstraint() }
-                    );
-
-
-            });
-        }
     }
 }
